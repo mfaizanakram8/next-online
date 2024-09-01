82bc13ea2552b58d6f99eb17303af3f467e4841c
mfaizanakram8 <mfaizanakram536@gmail.com>
2024-09-01 13:55:04 +0500

    update1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <div  className="flex items-center justify-center h-screen bg-gray-300">
       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 md:w-3/2 ">
-    <Image className="w-34 h-34 md:w-88 md:h-auto md:rounded-none rounded-full mx-auto" src="/home.jfif" alt="home" width="384" height="512"/>
+    <Image className="w-34 h-34 md:w-88 md:h-auto md:rounded-none rounded-full mx-auto" src="/home.png" alt="home" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-xl font-medium">
